f209a3eef57a05f2cbb27fe49d7f343db144d748
JP <jonathankbyers@gmail.com>
2024-03-10 18:50:58 -0400

    atl star

--- api-nasa-hubble/js/main.js
+++ api-nasa-hubble/js/main.js
@@ -1,8 +1,24 @@
-//Example fetch using pokemonapi.co
+
+const dateObj = new Date();
+const month   = dateObj.getUTCMonth() + 1; // months from 1-12
+const day     = dateObj.getUTCDate();
+const year    = dateObj.getUTCFullYear();
+
+// Using padded values, so that 2023/1/7 becomes 2023/01/07
+const pMonth = month.toString().padStart(2,"0");
+const pDay = day.toString().padStart(2,"0");
+
+// Using template literals:
+const newPaddedDate = `${year}-${pMonth}-${pDay}`;
+let choice = document.querySelector('input').value
+if(choice === '') getFetch() // if no value passed, get today
+else choice = newPaddedDate
+console.log('newPaddedDate'+newPaddedDate)
+
 document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
-  const choice = document.querySelector('input').value
+  choice = document.querySelector('input').value
   const url = `https://api.nasa.gov/planetary/apod?api_key=dqfKUbacyWxcvzrayCY2yOycvd8KxzjQmCTmgb82&date=${ choice }`
   console.log(choice)
 
@@ -11,7 +27,7 @@ function getFetch(){
       .then(data => {
         console.log(data)
         document.querySelector('img').src = data.hdurl
-        document.querySelector('h3').innerText = data.msg
+        document.querySelector('h3').innerText = data.explanation
       })
       .catch(err => {
           console.log(`error ${err}`)
